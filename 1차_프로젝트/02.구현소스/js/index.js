93d1f987329b994b409a287a64420f914812facf
ToffeeSta <Wjdgnsrh@gmail.com>
2024-12-06 17:22:16 +0900

    ㅇㅇ

--- 1차_프로젝트/02.구현소스/js/index.js
+++ 1차_프로젝트/02.구현소스/js/index.js
@@ -1,18 +1,21 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const video = document.querySelector('.sub1-video video');
-  const txtBox = document.querySelector('.sub1-area txt-box');
+document.addEventListener("DOMContentLoaded", () => {
+  const video = document.querySelector(".sub1-video video");
+  const txtBox = document.querySelector(".sub1-area txt-box");
 
-  const observer = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-      if (entry.isIntersecting && entry.intersectionRatio > 0.5) {
-        video.play(); // 비디오 재생
-      } else {
-        video.pause(); // 비디오 정지
-      }
-    });
-  }, {
-    threshold: [0.5] // 비디오 요소의 50%가 보이는 화면 기준
-  });
+  const observer = new IntersectionObserver(
+    (entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting && entry.intersectionRatio > 0.5) {
+          video.play(); // 비디오 재생
+        } else {
+          video.pause(); // 비디오 정지
+        }
+      });
+    },
+    {
+      threshold: [0.5], // 비디오 요소의 50%가 보이는 화면 기준
+    }
+  );
 
-  observer.observe(document.querySelector('.sub1-video'));
+  observer.observe(document.querySelector(".sub1-video"));
 });

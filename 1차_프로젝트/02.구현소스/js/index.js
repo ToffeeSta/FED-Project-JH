da900349496399cb5fd15ca7e09b0bedc3a53cda
Tom B. Parker <tombap8@hotmail.com>
2024-12-09 17:04:37 +0900

    ss

--- 1차_프로젝트/02.구현소스/js/index.js
+++ 1차_프로젝트/02.구현소스/js/index.js
@@ -1,27 +1,26 @@
 //////////// sub1-area 비디오 재생 //////////////
 document.addEventListener("DOMContentLoaded", () => {
   const video = document.querySelector(".sub1-video video");
-  const txtBox = document.querySelector(".sub1-area txt-box");
-  const tgBox = document.querySelector(".sub1-video");
-
-//   const vdo = $('.sub1-video video');
-// console.log(vdo);
-//   setTimeout(() => {
-//     vdo.get(0).play();
-    
-//   }, 1000);
-//   const winH = window.innerHeight;
-//   const getBCL = x=>x.getBoundingClientRect().top;
-//   let sts = true;
-
+  const txtBox = document.querySelector(".tbox1");
+  console.log(txtBox);
+  txtBox.style.display = "none";
+  
   const observer = new IntersectionObserver(
     (entries) => {
       entries.forEach((entry) => {
         if (entry.isIntersecting && entry.intersectionRatio > 0.5) {
           video.play(); // 비디오 재생
+          video.addEventListener('timeupdate',()=>{
+            console.log(video.currentTime);
+            if(video.currentTime>3){
+
+              txtBox.style.display = "block";
+            }
+          })
         } else {
           video.pause(); // 비디오 정지
           video.currentTime = 0; // 처음으로 돌아가기
+          txtBox.style.display = "none";
         }
       });
     },
@@ -31,22 +30,6 @@ document.addEventListener("DOMContentLoaded", () => {
   );
 
   observer.observe(document.querySelector(".sub1-video"));
-
-
-  // video.addEventListener('timeupdate', ()=>{
-  //   console.log('vvv');
-  // })
-
-  // window.addEventListener('scroll', ()=>{
-
-  //   if(this.scrollY < winH*0.5 && sts){
-  //     sts = false;
-  //     video.play();
-  //     video.currentTime = 0;
-  //   }
-  //   console.log(getBCL(tgBox),video,'/',winH*0.5,'/스크롤:',this.scrollY);
-  // })
-
 
 
 });

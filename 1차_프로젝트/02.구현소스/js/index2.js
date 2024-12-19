7ba5f5e26501609dd5147e3df9f163a7a0361a3d
ToffeeSta <Wjdgnsrh@gmail.com>
2024-12-19 12:03:17 +0900

    d

--- 1차_프로젝트/02.구현소스/js/index2.js
+++ 1차_프로젝트/02.구현소스/js/index2.js
@@ -33,4 +33,24 @@ document.addEventListener("DOMContentLoaded", () => {
     observer.observe(document.querySelector(".sub1-video"),);
   });
   
-  //
+  //////////////// 서브3(sec4) ///////////////
+const sec4MenuLi = document.querySelectorAll(".sec4-menu > ul > li");
+const sec4ConLi = document.querySelectorAll(
+  ".sub3-ban > .img-area > .item-list"
+);
+
+let num = 0;
+
+sec4MenuLi[num].classList.add("on");
+sec4ConLi[num].classList.add("on");
+
+sec4MenuLi.forEach((itme, index) => {
+  itme.onclick = () => {
+    sec4MenuLi[num].classList.remove("on");
+    sec4ConLi[num].classList.remove("on");
+
+    itme.classList.add("on");
+    sec4ConLi[index].classList.add("on");
+    num = index;
+  };
+});
